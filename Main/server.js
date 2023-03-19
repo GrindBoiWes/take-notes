0d8be0fb124ad123030b0fe433400e664079adf4
Wes Harrison <wesharrison89@gmail.com>
2023-03-19 11:36:53 -0700

    added app.get features to route html files when it is requested

--- Main/server.js
+++ Main/server.js
@@ -6,3 +6,15 @@ const {v4: uuidv4} = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'Main/public')));
+app.use(express.urlencoded({extended:true}));
+app.use((express.json()));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Main/public/notes.html'));
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Main/public/index.html'));
+})
+
