4c9bbdaf57e307286acfa5da016398c4b3f6448f
Wes Harrison <wesharrison89@gmail.com>
2023-03-20 21:32:20 -0700

    added notes throughout code, as well as added content to the readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # take-notes
+An app for keeping yourself on task
+
+## Description
+You may feel like there aren't enough hours in the day to get everything done. Well, you are correct. With this app, it will be easier to keep on top of those pesky tasks that are taking up your time. With Take Notes, simply enter in a title of your task, followed by a brief description and click that save button. Continue to add each task that is on your list of todo's. Once a task has been completed, simply click the trash icon located next to your new note (left hand side of the page), and now you will be ready to move onto your next task. Finish everything, or continuously add tasks as soon as it comes up.
+
+For running this app, you will need to run node server.js in the terminal, then follow the local host link inside to visit the page.
+
+## Screenshot / Gif
+
+## Links
+
+Render : https://take-notes-5fdl.onrender.com
+
+Github : https://github.com/GrindBoiWes/take-notes
+
+## Credits
+Source material from previous modules, as well as a few sessions with askbcs. Mostly fixing my typos..
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+// This section imports its corresponding module
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const {v4: uuidv4} = require('uuid');
-
+// Express creates a new application instance, while PORT sets the port number for the server
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+// This section will serve static files from the public folder, and then will use middleware functions for parsing the incoming requests
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({extended:true}));
 app.use((express.json()));
-
+// This section defines routes for serving the HTML files
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/assets/notes.html'));
 })
@@ -17,9 +18,9 @@ app.get('/notes', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/assets/index.html'));
 })
-
+// Empty array to store notes data
 const notes = [];
-
+// This section defines API endpoints for retrieving and creating data
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 })
@@ -33,7 +34,7 @@ app.post('/api/notes', (req, res) => {
     notes.push(newNote);
     res.json(newNote)
 })
-
+// Defines an API endpoint for deleting a note by the notes ID
 app.delete('/api/notes/:id', (req, res) => {
     const noteIndex = notes.findIndex(note => note.id === req.params.id);
         if (noteIndex !== -1) {
@@ -41,7 +42,7 @@ app.delete('/api/notes/:id', (req, res) => {
         }
         res.sendStatus(204);
 });
-
+// This section listens to the PORT the page is being deployed on
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
